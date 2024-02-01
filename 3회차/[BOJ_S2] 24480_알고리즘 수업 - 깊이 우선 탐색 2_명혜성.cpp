7cfce7b10b5367625500042798bfe152ef882e2f
Hyeseong-Myeong <a01025819869@gmail.com>
2024-02-01 15:50:47 +0900

    solved: week2_2

---
+++ 3회차/[BOJ_S2] 24480_알고리즘 수업 - 깊이 우선 탐색 2_명혜성.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001] = { 0, };
+int result[100001];
+int cnt = 0;
+
+
+void dfs(int R) {
+	if (visited[R] == 1)
+		return;
+	else {
+		visited[R] = 1;
+		result[R] = ++cnt;
+
+		for (int i = 0; i < graph[R].size(); i++) {
+			dfs(graph[R][i]);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+
+	for (int i = 1; i <= M; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		sort(graph[i].begin(), graph[i].end(), greater<int>());
+	}
+
+	dfs(R);
+
+	for (int i = 1; i <= N; i++) {
+		printf("%d\n", result[i]);
+	}
+
+}

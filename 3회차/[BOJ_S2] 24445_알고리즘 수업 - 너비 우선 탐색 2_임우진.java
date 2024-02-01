f2ed78ee1e4a884316adc4d138fb85436473b803
WooJinLim <95209789+woojin2296@users.noreply.github.com>
2024-02-01 17:14:07 +0900

    solved: week 3 solved

---
+++ 3회차/[BOJ_S2] 24445_알고리즘 수업 - 너비 우선 탐색 2_임우진.java
@@ -0,0 +1,59 @@
+package 알고리즘;
+
+import java.util.*;
+
+public class Main24445 {
+    static int n;
+    static int m;
+    static int r;
+
+    static int count = 1;
+    static int[] visited;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static Queue<Integer> queue = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String[] input1 = sc.nextLine().split(" ");
+        n = Integer.parseInt(input1[0]);
+        m = Integer.parseInt(input1[1]);
+        r = Integer.parseInt(input1[2]);
+
+        visited = new int[n+1];
+        for (int i = 0; i < n + 1; i ++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i ++) {
+            String[] input = sc.nextLine().split(" ");
+
+            graph.get(Integer.parseInt(input[0])).add(Integer.parseInt(input[1]));
+            graph.get(Integer.parseInt(input[1])).add(Integer.parseInt(input[0]));
+        }
+        
+        for (int i = 1; i < n + 1; i++) {
+            graph.get(i).sort(Comparator.naturalOrder());
+        }
+        
+        bfs(r);
+
+        for (int i = 1; i < n + 1; i++) {
+            System.out.println(visited[i]);
+        }
+    }
+
+    private static void bfs(int node) {
+        queue.offer(node);
+        visited[node] = count++;
+
+        while(!queue.isEmpty()){
+            int a = queue.poll();
+            for(int num : graph.get(a)) {
+                if (visited[num] != 0) continue;
+                queue.add(num);
+                visited[num] = count++;
+            }
+        }
+    }
+}
